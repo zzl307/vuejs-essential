81b0969673922c71b92cbac89e8ea0f6966d7e07
vagrant <vagrant@homestead>
2020-11-04 01:38:08 +0000

    登录注册

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,82 @@
+<template>
+<div class="navbar navbar-default topnav">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" @click="toggleNav">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <router-link to="/" class="navbar-brand">
+                <span class="title">{{ logo.title }}</span>
+                <img :src="logo.src" :alt="logo.title">
+            </router-link>
+        </div>
+
+        <div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', { in:showCollapsedNav }]">
+            <ul class="nav navbar-nav">
+                <li v-for="(item, index) in navList" :key="item.nav" :class="{ active: index === activeNavIndex }">
+                    <a href="#" @click="changeNavIndex(index)">{{ item.nav }}</a>
+                </li>
+            </ul>
+
+            <div class="navbar-right">
+                <TheEntry />
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import TheEntry from '@/components/layouts/TheEntry'
+
+export default {
+    name: 'TheHeader',
+    components: {
+        TheEntry
+    },
+    data() {
+        return {
+            logo: {
+                src: `${this.uploadsUrl}communities/hIZjRRdF8oVYZy69XJnT.png`,
+                title: 'Learnku Vue.js'
+            },
+            navList: [{
+                nav: '社区'
+            }, {
+                nav: '头条'
+            }, {
+                nav: '问答'
+            }, {
+                nav: '教程'
+            }],
+            activeNavIndex: 0,
+            showCollapsedNav: false
+        }
+    },
+    beforeCreate() {
+        this.uploadsUrl = 'https://cdn.learnku.com/uploads/'
+    },
+    methods: {
+        changeNavIndex(index) {
+            this.activeNavIndex = index
+        },
+        toggleNav() {
+            this.showCollapsedNav = !this.showCollapsedNav
+        }
+    },
+}
+</script>
+
+<style scoped>
+.title {
+    display: none;
+}
+
+.navbar-default .navbar-nav>.active>a {
+    background: rgba(0, 0, 0, .03);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import './directives'
+import './components'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
